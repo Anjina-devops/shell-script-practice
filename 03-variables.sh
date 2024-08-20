884f9bbfe2a6f4885a69e1f9ed5a6bad91a2b568
Anjina-devops <anjina0322@gmil.com>
2024-08-20 14:12:09 +0530

    added 2nd script file

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo "Hello i am on devops"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hello ...$PERSON2"
+echo "$PERSON2 : Hi..$PERSON1"
+echo "$PERSON1 : how are you $PERSON2"
+echo "$PERSON2 : I am fine ...what about you $PERSON1"
