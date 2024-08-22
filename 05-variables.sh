7976c8be447fb31c07bafe3d96a5d001e356e141
Anjina-devops <anjina0322@gmil.com>
2024-08-22 10:19:13 +0530

    sample programm

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# this is the example to read input from prompt farmat through cmd 
+
+echo "Please enter your username :"
+read username
+echo "your entered username is :$username"
+
+echo "Please enter your password"
+read pwd
+echo "your entered password is :$pwd"
