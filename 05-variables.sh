5cb68da1e6a5fe6728cdf74d118e13d85151fdb0
Anjina-devops <anjina0322@gmil.com>
2024-08-22 10:21:27 +0530

    modified this fiel

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,9 +3,9 @@
 # this is the example to read input from prompt farmat through cmd 
 
 echo "Please enter your username :"
-read username
+read -s username
 echo "your entered username is :$username"
 
 echo "Please enter your password"
-read pwd
+read -s pwd #if you mentioned  -s it will hide the entered details
 echo "your entered password is :$pwd"
